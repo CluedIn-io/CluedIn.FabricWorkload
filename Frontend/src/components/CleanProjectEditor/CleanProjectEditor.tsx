8cd20d637757b709fac5bf0048c10700f4ab4127
Ho Peng Foong <pfh@cluedin.com>
2025-05-19 09:15:09 +0800

    fix: Add missing title

--- Frontend/src/components/CleanProjectEditor/CleanProjectEditor.tsx
+++ Frontend/src/components/CleanProjectEditor/CleanProjectEditor.tsx
@@ -624,6 +624,7 @@ export function CleanProjectEditor(props: PageProps) {
               }}>
               <DialogSurface>
                 <DialogTitle>
+                  Launch In CluedIn
                 </DialogTitle>
                 <DialogBody>
                   <DialogContent >
