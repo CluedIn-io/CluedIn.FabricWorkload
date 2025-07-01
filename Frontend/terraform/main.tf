50e8fc7ef335f2bf4e538f6c87e87b180bd9878a
imtiyaz shaik <mis@cluedin.com>
2025-07-02 03:07:19 +0800

    fix: storage accounnt

--- Frontend/terraform/main.tf
+++ Frontend/terraform/main.tf
@@ -6,3 +6,19 @@ resource "azurerm_resource_group" "group" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_storage_account" "fabric_ui" {
+  name                     = "fabricuistoragefrontend"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  static_website {
+    index_document = "index.html"
+    error_404_document = "index.html"
+  }
+}
+
+output "web_endpoint" {
+  value = azurerm_storage_account.fabric_ui.primary_web_endpoint
+}
