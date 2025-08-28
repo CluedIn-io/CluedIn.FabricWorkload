08535d72836fcfc619383831c857337d0e3f8035
imtiyaz shaik <mis@cluedin.com>
2025-08-28 18:10:29 +0800

    fix: added backedn

--- Backend/terraform/main.tf
+++ Backend/terraform/main.tf
@@ -1,86 +1,25 @@
-locals {
-  app_package_md5    = filemd5(var.function_app_package)
+provider "azurerm" {
+  features {}
 }
 
-# Resource group
-resource "azurerm_resource_group" "group" {
-  name     = var.resource_group
-  location = var.location
+resource "azurerm_container_app_environment" "app_env" {
+  name                = "backend-env"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
-# Storage account
-resource "azurerm_storage_account" "store" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.group.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "RAGRS"
-}
+resource "azurerm_container_app" "backend" {
+  name                         = "backend-api"
+  resource_group_name          = var.resource_group_name
+  container_app_environment_id = azurerm_container_app_environment.app_env.id
+  revision_mode                = "Single"
 
-# App Insights / Workspace
-resource "azurerm_log_analytics_workspace" "workspace" {
-  name                = "la-${var.function_app_name}"
-  location            = azurerm_resource_group.group.location
-  resource_group_name = azurerm_resource_group.group.name
-  retention_in_days   = 30
-}
-
-resource "azurerm_application_insights" "insights" {
-  name                = "in-${var.function_app_name}"
-  location            = azurerm_resource_group.group.location
-  resource_group_name = azurerm_resource_group.group.name
-  application_type    = "web"
-  workspace_id        = azurerm_log_analytics_workspace.workspace.id
-}
-
-# App plan
-resource "azurerm_service_plan" "plan" {
-  name                = var.app_service_plan
-  resource_group_name = azurerm_resource_group.group.name
-  location            = azurerm_resource_group.group.location
-  os_type             = "Linux"
-  sku_name            = "S1"
-}
-
-resource "azurerm_linux_function_app" "webhook" {
-  name                = var.function_app_name
-  resource_group_name = azurerm_resource_group.group.name
-  location            = azurerm_resource_group.group.location
-
-  storage_account_name       = azurerm_storage_account.store.name
-  storage_account_access_key = azurerm_storage_account.store.primary_access_key
-
-  service_plan_id = azurerm_service_plan.plan.id
-
-  site_config {
-    application_insights_connection_string    = azurerm_application_insights.insights.connection_string
-    application_insights_key                  = azurerm_application_insights.insights.instrumentation_key
-    always_on                              = true
-
-    application_stack {
-      dotnet_version              = "8.0"
-      use_dotnet_isolated_runtime = true
+  template {
+    container {
+      name   = "backend-api"
+      image  = var.docker_image
+      cpu    = 0.5
+      memory = "1.0Gi"
     }
   }
-
-  zip_deploy_file             = var.function_app_package
-  
-  app_settings = {
-    WEBSITE_RUN_FROM_PACKAGE                  = 1
-    QueueEmailRequests                        = var.queue_name_email_requests
-    QueueLifeCycleEvents                      = var.queue_name_life_cycle_events
-    QueueLifeCycleActions                     = var.queue_name_life_cycle_actions
-    QueueInfrastructureAlertActions           = var.queue_name_infrastructure_alerts
-    EmailerBaseUrl                            = var.emailer_base_url
-    EmailerApiKey                             = var.emailer_api_key
-    BillingBaseUrl                            = var.billingBaseUrl
-    BillingApiKey                             = var.billingApiKey
-    AutomationWebhookUrl                      = var.automationWebhookUrl
-    LifeCycleWebhookUnsubscribe               = var.lifecycle_webhook_unsubscribe
-    LifeCycleWebhookRemove                    = var.lifecycle_webhook_remove
-    LifeCycleWebhookScale                     = var.lifecycle_webhook_scale
-    SlackUrl                                  = var.slackUrl
-    DefaultReleaseVersion                     = var.defaultReleaseVersion
-    ENVIRONMENT                               = var.environment
-  }     
 }
--- Backend/terraform/variables.tf
+++ Backend/terraform/variables.tf
@@ -1,19 +1,3 @@
-# General
-variable "environment" {
-  type = string
-}
-
-# Resource group
-variable "resource_group" {
-  type = string
-}
-
-# Location
-variable "location" {
-  type = string
-}
-
-# The following are required for the azurerm provider
 variable "azure_subscription_id" {
   type = string
 }
@@ -31,82 +15,21 @@ variable "azure_tenant_id" {
   type = string
 }
 
-variable "app_service_plan" {
+# General
+variable "environment" {
   type = string
 }
 
-variable "function_app_name" {
+# Location
+variable "location" {
   type = string
 }
 
-variable "function_app_package" {
+# Resource group
+variable "resource_group_name" {
   type = string
 }
 
-variable "storage_account_name" {
-  type = string
-}
-
-variable "container_name" {
-  type = string
-}
-
-variable "emailer_base_url" {
-  type = string
-}
-
-variable "emailer_api_key" {
-  type = string
-}
-
-variable "service_bus_name" {
-  type = string
-}
-
-variable "queue_name_email_requests" {
-  type = string
-}
-
-variable "queue_name_life_cycle_events" {
-  type = string
-}
-
-variable "queue_name_life_cycle_actions" {
-  type = string
-}
-
-variable "queue_name_infrastructure_alerts" {
-  type = string
-}
-
-variable "billingBaseUrl" {
-  type = string
-}
-
-variable "billingApiKey" {
-  type = string
-}
-
-variable "automationWebhookUrl" {
-  type = string
-}
-
-variable "lifecycle_webhook_unsubscribe" {
-  type = string
-}
-
-variable "lifecycle_webhook_remove" {
-  type = string
-}
-
-variable "lifecycle_webhook_scale" {
-  type = string
-}
-
-variable "slackUrl" {
-  type = string
-}
-
-variable "defaultReleaseVersion" {
+variable "docker_image" {
   type = string
 }
