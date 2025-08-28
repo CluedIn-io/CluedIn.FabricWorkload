451acafccd9b2738fc67ff0ed00d2d214d0bc203
imtiyaz shaik <mis@cluedin.com>
2025-08-28 23:06:24 +0800

    fix: fixing image and provider

--- Backend/terraform/providers.tf
+++ Backend/terraform/providers.tf
@@ -7,10 +7,8 @@ terraform {
   }
   required_providers {
     azurerm = {
-      version = "3.17.0"
-    }
-    azuread = {
-      version = "2.22.0"
+      source  = "hashicorp/azurerm"
+      version = ">=3.75.0"
     }
   }
 }
