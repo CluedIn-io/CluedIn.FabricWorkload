2fffd3ba0bb7a55d70ec8fe9c60e723636654910
imtiyaz shaik <mis@cluedin.com>
2025-08-29 20:43:09 +0800

    fix: changes

--- Backend/terraform/variables.tf
+++ Backend/terraform/variables.tf
@@ -2,15 +2,6 @@ variable "azure_subscription_id" {
   type = string
 }
 
-variable "azure_client_id" {
-  type = string
-}
-
-variable "azure_client_secret" {
-  type      = string
-  sensitive = true
-}
-
 variable "azure_tenant_id" {
   type = string
 }
