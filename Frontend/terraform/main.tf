348364ab2a82444605ae20129943c1e321885350
imtiyaz shaik <mis@cluedin.com>
2025-09-16 03:26:53 +0800

    fix: frontend

--- Frontend/terraform/main.tf
+++ Frontend/terraform/main.tf
@@ -121,6 +121,8 @@ resource "azapi_resource" "frontend_cert" {
   type      = "Microsoft.App/managedEnvironments/certificates@2023-05-01"
   name      = "fabric-ui-cert"
   parent_id = azurerm_container_app_environment.app_env.id
+  location  = azurerm_container_app_environment.app_env.location
+  schema_validation_enabled = false
 
   body = jsonencode({
     properties = {
