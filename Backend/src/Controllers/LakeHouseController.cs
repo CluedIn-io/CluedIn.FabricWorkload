453072d9bfc6f4d2d43b2c98644bfd5ab2133d7f
Ho Peng Foong <pfh@cluedin.com>
2025-09-19 00:24:15 +0800

    scope

--- Backend/src/Controllers/LakeHouseController.cs
+++ Backend/src/Controllers/LakeHouseController.cs
@@ -192,7 +192,7 @@ namespace Boilerplate.Controllers
         [HttpPost("connections/test")]
         public async Task<IActionResult> TestConnectionAsync([FromBody] CluedInConnection connection)
         {
-            _ = await _authenticationService.AuthenticateDataPlaneCall(_httpContextAccessor.HttpContext, allowedScopes: []);
+            _ = await _authenticationService.AuthenticateDataPlaneCall(_httpContextAccessor.HttpContext, allowedScopes: ScopesForReadLakehouseFile);
             try
             {
                 var requestUri = new Uri($"https://{connection.OrganizationName}.{connection.Domain}/auth/connect/token");
@@ -233,7 +233,7 @@ namespace Boilerplate.Controllers
         [HttpPost("organizations")]
         public async Task<IActionResult> CreateOrganizationsAsync([FromBody] CreateOrganizationRequest createOrganizationRequest)
         {
-            _ = await _authenticationService.AuthenticateDataPlaneCall(_httpContextAccessor.HttpContext, allowedScopes: []);
+            _ = await _authenticationService.AuthenticateDataPlaneCall(_httpContextAccessor.HttpContext, allowedScopes: ScopesForReadLakehouseFile);
             var cluedInDomain = Environment.GetEnvironmentVariable("CluedInDomain");
             var newAccountAccessKey = Environment.GetEnvironmentVariable("CluedInNewAccountAccessKey");
             var request = new HttpRequestMessage(HttpMethod.Post, $"https://app.{cluedInDomain}/auth/api/account/new");
